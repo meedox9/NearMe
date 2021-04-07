3c646ba45c699c2c7ade1801cfc83791fe4c8f33
Hirthik Bhojwani <57116500+hbhojwani@users.noreply.github.com>
2021-04-07 14:14:26 -0230

    Support Page Addition

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ContactForm from "./ContactForm";
+import "./Contact.css";
+import Header from "./../menu/Header";
+import SupportPic from "./../../images/contactimage.png";
+
 
 function Contact() {
   return (
     <div>
+      <Header/>
       <ContactForm />
     </div>
   );
